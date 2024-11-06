189107b6a2a1b6ce1c600e068b6c8b75a5e5c3a0
Jörn Dombrowski <j.dombrowski@handelsblattgroup.com>
2024-11-06 17:59:40 +0100

    feat: switch to content-type application/octet-stream and http method put

--- pkg/rest/server.go
+++ pkg/rest/server.go
@@ -29,11 +29,10 @@ func NewServer() *Server {
 			Token:  os.Getenv("TELEGRAM_BOT_TOKEN"),
 		}),
 	}
-
 }
 
 func (srv Server) Start() {
-	http.HandleFunc("/upload", srv.uploadHandler)
+	http.HandleFunc("/upload/", srv.uploadHandler)
 
 	fmt.Println("Server startet auf Port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -42,8 +41,8 @@ func (srv Server) Start() {
 // uploadHandler verarbeitet den Datei-Upload
 func (srv Server) uploadHandler(w http.ResponseWriter, r *http.Request) {
 	// Überprüfen, ob es ein POST-Request ist
-	if r.Method != http.MethodPost {
-		http.Error(w, "Nur POST-Methoden sind erlaubt", http.StatusMethodNotAllowed)
+	if r.Method != http.MethodPut {
+		http.Error(w, "Only PUT methods are allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
@@ -55,17 +54,11 @@ func (srv Server) uploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Datei aus dem Request extrahieren
-	file, header, err := r.FormFile("file")
-	if err != nil {
-		log.Println(err)
-		http.Error(w, "Konnte Datei nicht lesen", http.StatusBadRequest)
-		return
-	}
-	defer file.Close()
+	// Extrahieren des Dateinamens aus der URL
+	fileName := r.URL.Path[len("/upload/"):]
 
-	// Speichert die Datei in einem Ordner
-	filePath := "./uploads/" + header.Filename
+	// Datei aus dem Request extrahieren
+	filePath := "./uploads/" + fileName
 	outFile, err := os.Create(filePath)
 	if err != nil {
 		log.Println(err)
@@ -75,7 +68,7 @@ func (srv Server) uploadHandler(w http.ResponseWriter, r *http.Request) {
 	defer outFile.Close()
 
 	// Kopiert den Dateiinhalt
-	_, err = io.Copy(outFile, file)
+	_, err = io.Copy(outFile, r.Body)
 	if err != nil {
 		http.Error(w, "Fehler beim Speichern der Datei", http.StatusInternalServerError)
 		return
@@ -85,7 +78,7 @@ func (srv Server) uploadHandler(w http.ResponseWriter, r *http.Request) {
 	go srv.processSummary(filePath)
 
 	// Erfolgsnachricht zurücksenden
-	fmt.Fprintf(w, "Datei %s wurde erfolgreich hochgeladen", header.Filename)
+	fmt.Fprintf(w, "Datei %s wurde erfolgreich hochgeladen", fileName)
 }
 
 func (srv Server) processSummary(filePath string) {
