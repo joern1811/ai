a1380ef9f0184cec40ebed0bde474cba0714fe20
Jörn Dombrowski <j.dombrowski@handelsblattgroup.com>
2025-03-25 18:33:45 +0100

    fix: Fix minor typo in speach command synopsis
    Removed an unnecessary period from the 'Short' field of the speach command to ensure consistent formatting across command descriptions. This change does not affect functionality but improves textual consistency.

--- pkg/cmd/speach.go
+++ pkg/cmd/speach.go
@@ -4,7 +4,7 @@ import "github.com/spf13/cobra"
 
 var speachCmd = &cobra.Command{
 	Use:   "speach",
-	Short: "Some tools in the context of speach.",
+	Short: "Some tools in the context of speach",
 }
 
 func init() {
